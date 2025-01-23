67469c2c41d5c37acd2f63863b618b92e7dda218
Adrian Hirt <100226573+admarhi@users.noreply.github.com>
2025-01-23 16:20:56 -0500

    new deps

--- R/translator-package.R
+++ R/translator-package.R
@@ -3,15 +3,18 @@
 
 ## usethis namespace: start
 #' @importFrom dplyr across
+#' @importFrom dplyr group_by
 #' @importFrom dplyr if_else
 #' @importFrom dplyr mutate
+#' @importFrom dplyr pull
+#' @importFrom dplyr summarise
 #' @importFrom httr2 req_body_form
 #' @importFrom httr2 req_headers
 #' @importFrom httr2 req_perform
 #' @importFrom httr2 request
+#' @importFrom httr2 resp_body_json
 #' @importFrom purrr list_rbind
 #' @importFrom purrr map
 #' @importFrom tibble as_tibble
-#' @importFrom tibble resp_body_json
 ## usethis namespace: end
 NULL
